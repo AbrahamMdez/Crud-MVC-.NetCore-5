543a1c7e75f0d4ef0ca52096c9d38f33b63b0bd0
Abraham <nencho_1@hotmail.com>
2021-06-06 12:12:15 +0200

    Add project files.

---
+++ Crud MVC .NetCore 5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Crud_MVC_.NetCore_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

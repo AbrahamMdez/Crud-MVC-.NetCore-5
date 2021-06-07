fc7d0c77d1eef67abcab796d9a49366865d1f75e
Abraham <nencho_1@hotmail.com>
2021-06-07 12:43:16 +0200

    Created book controller

---
+++ Crud MVC .NetCore 5/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Crud_MVC_.NetCore_5.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_MVC_.NetCore_5.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

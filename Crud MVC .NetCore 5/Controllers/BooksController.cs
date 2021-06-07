646a452d2ca84b9c50f4c25d707539d02ac71389
Abraham <nencho_1@hotmail.com>
2021-06-07 14:33:02 +0200

    Finished all operations

--- Crud MVC .NetCore 5/Controllers/BooksController.cs
+++ Crud MVC .NetCore 5/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
 ﻿using Crud_MVC_.NetCore_5.Data;
+using Crud_MVC_.NetCore_5.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,7 +19,96 @@ namespace Crud_MVC_.NetCore_5.Controllers
 
         public IActionResult Index()
         {
+            IEnumerable<Book> bookList = _context.Book;
+            return View(bookList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Book.Add(book);
+                _context.SaveChanges();
+
+                TempData["message"] = "El libro se creó correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var book = _context.Book.Find(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Book.Update(book);
+                _context.SaveChanges();
+
+                TempData["message"] = "El libro se actualizó correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var book = _context.Book.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteBook(int? id)
+        {
+
+            var book = _context.Book.Find(id);
+
+            if(book == null)
+            {
+                return NotFound();
+            }
+;
+           
+            _context.Book.Remove(book);
+            _context.SaveChanges();
+
+            TempData["message"] = "El libro se borró correctamente";
+            return RedirectToAction("Index");
         }
     }
 }
